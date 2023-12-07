b50ebb6a4f9a15e773588a490fa389f445a731b6
Manish Thulung <manish.thulung58@gmail.com>
2023-12-07 23:41:36 +0545

    theme toggler

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
       <div className="text-4xl font-bold">this is protected route</div>
     </>
   );
