b3f2fb12876b9e516ea4007b0be0de00d49b68f4
Manish Thulung <manish.thulung58@gmail.com>
2023-12-15 23:51:40 +0545

    feat: sidebar, modal and invitation link

---
+++ components/modals/test-modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { RefreshCw } from "lucide-react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "@/components/ui/button";
+
+const TestModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "test";
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Testing Testing Yo Yo!{" "}
+          </DialogTitle>
+        </DialogHeader>
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+
+          <Button
+            variant="link"
+            size="sm"
+            className="text-xs text-zinc-500 mt-4"
+          >
+            Generate a new link
+            <RefreshCw className="w-4 h-4 ml-2" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TestModal;
