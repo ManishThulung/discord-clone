a33b53002b611a70294099250efa7686c6063080
Manish Thulung <manish.thulung58@gmail.com>
2023-12-26 20:57:06 +0545

    feat: chat setup

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-function page() {
-  return <div>server id page</div>;
+async function page({ params }: { params: { serverId: string } }) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findFirst({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile?.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel.id}`);
 }
 
 export default page;
